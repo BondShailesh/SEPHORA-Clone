3b9803e9d387129af1bbca78df0cbf4209e32bb0
BondShailesh <Shaileshrajvicky@gmail.com>
2022-04-01 08:58:44 +0530

    header and footer is approximetly done

---
+++ index.js
@@ -0,0 +1,78 @@
+document.querySelector("#nnnow").addEventListener("click",funnnnow)
+function funnnnow(){
+    window.location.href = "topicon/nnnow.html"
+}
+
+document.querySelector("#iconlocator").addEventListener("click",funiconlocator)
+function funiconlocator(){
+    window.location.href = "topicon/iconlocator.html"
+}
+
+document.querySelector("#storelocator").addEventListener("click",funstorelocator)
+function funstorelocator(){
+    window.location.href = "topicon/iconlocator.html"
+    document.querySelector("#storelocator").style = "pointer"
+}
+
+document.querySelector("#checkoutText").addEventListener("click",funcheckoutText)
+function funcheckoutText(){
+    window.location.href = "topicon/checkoutText.html"
+}
+
+document.querySelector(".getappicon").addEventListener("click",fungetappicon)
+function fungetappicon(){
+    window.location.href = "topicon/getapp.html"
+}
+
+document.querySelector("#getapp").addEventListener("click",fungetapp)
+function fungetapp(){
+    window.location.href = "topicon/getapp.html"
+}
+
+document.querySelector(".trackordericon").addEventListener("click",funtrackordericon)
+function funtrackordericon(){
+    window.location.href = "topicon/trackordericon.html"
+}
+
+document.querySelector("#trackorder").addEventListener("click",funtrackorder)
+function funtrackorder(){
+    window.location.href = "topicon/trackordericon.html"
+}
+
+
+document.querySelector(".loyaltyicon").addEventListener("click",funloyaltyicon)
+function funloyaltyicon(){
+    window.location.href = "topicon/loyaltyicon.html"
+}
+document.querySelector("#loyalty").addEventListener("click",funloyalty)
+function funloyalty(){
+    window.location.href = "topicon/loyaltyicon.html"
+}
+document.querySelector(".search").addEventListener("click",funsearch)
+function funsearch(){
+    window.location.href = "topicon/search.html"
+}
+
+document.querySelector("#sephora").addEventListener("click",funsephora)
+function funsephora(){
+    window.location.href = "index.html"
+}
+
+document.querySelector(".favourite").addEventListener("click",funfavourite)
+function funfavourite(){
+    window.location.href = "topicon/favourite.html"
+}
+document.querySelector(".bag").addEventListener("click",funbag)
+function funbag(){
+    window.location.href = "topicon/bag.html"
+}
+document.querySelector(".loginicon").addEventListener("click",funloginicon)
+function funloginicon(){
+    window.location.href = "topicon/loginicon.html"
+}
+
+document.querySelector("#login").addEventListener("click",funlogin)
+function funlogin(){
+    window.location.href = "topicon/loginicon.html"
+}
+
