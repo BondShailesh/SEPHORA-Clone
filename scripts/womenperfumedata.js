d38cd51a7b65cdc62986e8085d1e6a98a365cfbb
Jayesh Badarkhe <your github email>
2022-04-02 17:29:27 +0530

    kuchv git commit -mkuchv

---
+++ scripts/womenperfumedata.js
@@ -0,0 +1,274 @@
+ var data=[
+    {
+        img:" https://cdn05.nnnow.com/web-images/preview/styles/UMFPULC18J8/1604658376930/1.jpg ",
+        brand:"KENZO",
+        price:4625,
+        description: " World Eau De Toilette",
+        promotion:"BUY FOR 4500 AND GET A MASSAGE BALL AND GLOVES FREE",
+    },
+
+    {   
+        img:"https://cdn00.nnnow.com/web-images/preview/styles/N9JWX0CNPJ9/1631616174860/1.jpg",
+      
+        brand:"KAYALI",
+        price:10300,
+        description:"Kayali Vanilla Eau De Parfum",
+        promotion:"BUY FOR 4500 AND GET A MASSAGE BALL AND GLOVES FREE",
+    
+    },
+    
+    {
+        img:"https://cdn03.nnnow.com/web-images/preview/styles/KE8R7P6MPX8/1604658376917/1.jpg",
+        brand:"KENZO",
+        price:5900,
+        description: " World Eau De Parfum Intense ",
+    }, 
+
+    {
+        img:"https://cdn00.nnnow.com/web-images/preview/styles/XL27VHQEIMT/1645680546949/1.jpg",
+        brand:"GIVENCHY",
+        price:8075,
+        description: "  L'Interdit Eau De Parfum Rouge ",
+    },
+    {
+        img:"https://cdn02.nnnow.com/web-images/preview/styles/RMDAZ5IACI7/1495706609393/1.jpg",
+        brand:"GIVENCHY",
+        price:10100,
+        description: "Dahlia Divin Eau De Parfum",
+    },
+    {
+        img:"https://cdn01.nnnow.com/web-images/preview/styles/H730W8VP1UI/1580453335495/1.jpg",
+        brand:"GIVENCHY",
+        price:7175,
+        description: "  L'Interdit Eau De Parfum Rouge ",
+    },
+    
+    {
+        img:"https://cdn12.nnnow.com/web-images/preview/styles/4O91OGGF6Z7/1592541998240/1.jpg",
+        brand:"BURBERRY",
+        price:5300,
+        description: "Her Eau De Parfum"},
+
+    
+{
+    img:"https://cdn00.nnnow.com/web-images/large/styles/HJR8CMWBNQA/1633934870094/1.jpg",
+    brand:"KAYALI",
+    price:2200,
+    description:"Déjà Vu White Flower 57 Eau De Parfum",
+    promotion:"BUY FOR 4500 AND GET A MASSAGE BALL AND GLOVES FREE",
+},
+{
+  
+    img:"https://cdn04.nnnow.com/web-images/preview/styles/TD2KOAT71NK/1633934988044/1.jpg",
+    brand:"KAYALI",
+    price:10300,
+    description:"Déjà Vu White Flower 57 Eau De Parfum",
+    promotion:"BUY FOR 4500 AND GET A MASSAGE BALL AND GLOVES FREE",
+},
+{
+    img:"https://cdn14.nnnow.com/web-images/large/styles/VK36739SMZ7/1608276299525/1.jpg",
+    brand:"KAYALI",
+    price:2400,
+    description:"Déjà Vu White Flower 57 & Invite Only Amber 23 Eau De Parfum Rollerball",
+    promotion:"BUY FOR 4500 AND GET A MASSAGE BALL AND GLOVES FRE",
+},
+
+{
+    img:"https://cdn11.nnnow.com/web-images/preview/styles/NV2BU2YUTRJ/1608274933786/1.jpg",
+    brand:"KAYALI",
+    price:8700,
+    description:"Sweet Diamond Pink Pepper 25 Eau De Parfum Intense",
+    promotion:"BUY FOR 4500 AND GET A MASSAGE BALL AND GLOVES FREE",
+
+},
+{
+    img:"https://cdn00.nnnow.com/web-images/preview/styles/RAMPX81CPYL/1604476359457/1.jpg",
+    brand:"BURBERRY",
+    price:7800,
+    description: "My Burberry Eau De Parfum",
+},
+{
+    img:"https://cdn12.nnnow.com/web-images/preview/styles/4O91OGGF6Z7/1592541998240/1.jpg",
+    brand:"BURBERRY",
+    price:5300,
+    description: "Her Eau De Parfum  ",
+},
+{
+    img:"https://cdn04.nnnow.com/web-images/preview/styles/4JGNE3D0WPK/1604582394827/1.jpg",
+    brand:"BURBERRY",
+    price:4150,
+    description: "My Burberry Eau De Parfum ",
+},
+{
+    img:"https://cdn15.nnnow.com/web-images/preview/styles/KMD9ASCOK68/1604582394823/1.jpg",
+    brand:"BURBERRY",
+    price:5850,
+    description: " My Burberry Eau De Parfum ",
+},
+{
+    
+        img:"https://cdn15.nnnow.com/web-images/preview/styles/KMD9ASCOK68/1604582394823/1.jpg",
+        brand:"BURBERRY",
+        price:5850,
+        description: " My Burberry Eau De Parfum ",
+    },
+    {
+    img:"https://cdn16.nnnow.com/web-images/preview/styles/4V3IQKNUL0W/1632206998134/1.jpg",
+    brand:"BURBERRY",
+    price:4600,
+    description: " Brit Sheer Eau De Toilette ",
+},
+{
+    img:"https://cdn05.nnnow.com/web-images/preview/styles/90FPM84VYXG/1580465663359/1.jpg",
+    brand:"BURBERRY",
+    price:6200,
+    description: " Body Women Eau De Parfum ",
+    promotion:"BUY FOR 4500 AND GET A MASSAGE BALL AND GLOVES FRE",
+
+},
+{
+    img:"https://cdn10.nnnow.com/web-images/preview/styles/0RD8CV59U4F/1604658220733/2.jpg",
+    brand:"KENZO",
+    price:4625,
+    description: "Flower Eau De Parfum",
+},
+{
+    img:"https://cdn07.nnnow.com/web-images/preview/styles/6QVCHIMIIB1/1604552258710/1.jpg",
+    brand:"KENZO",
+    price:7200,
+    description: "Flower Eau De Parfum",
+},
+{
+    img:"https://cdn18.nnnow.com/web-images/preview/styles/CBXX85PS8LD/1604658220746/1.jpg",
+    brand:"KENZO",
+    price:3100,
+    description: " Totem Yellow Eau De Toilette ",
+},
+{
+    img:"https://cdn12.nnnow.com/web-images/preview/styles/C8Z1WRWFVIY/1604552258749/1.jpg",
+    brand:"KENZO",
+    price:4650,
+    description: " Aqua Pour Femme Eau De Toilette",
+},
+
+{
+    img:"https://cdn00.nnnow.com/web-images/preview/styles/XL27VHQEIMT/1645680546949/1.jpg",
+    brand:"GIVENCHY",
+    price:6057,
+    description: "Ange Ou Etrange Le Parfum",
+},
+{
+    img:"https://cdn07.nnnow.com/web-images/preview/styles/ALVMEWKNHGM/1495712465916/1.jpg",
+    brand:"GIVENCHY",
+    price:9650,
+    description: "Live Irresistible Eau De ParfumGivenchy Live Irresistible Eau De ParfumGivenchy Live Irresistible Eau De Parfum",
+},
+{
+    img:"https://cdn14.nnnow.com/web-images/preview/styles/STJX5BCPY3Y/1553169800941/1.jpg",
+    brand:"GIVENCHY",
+    price:6700,
+    description: "D Div Eau De Toilette InitialeGivenchy D Div Eau De Toilette Initial"
+},
+{
+    img:"https://cdn16.nnnow.com/web-images/preview/styles/IYKZK4Z0CCF/1610348954676/1.jpg",
+    brand:"BVLGARI",
+    price:7000,
+    description: "Omnia Coral Eau De Toilette"
+},
+{
+    img:"https://cdn06.nnnow.com/web-images/preview/styles/N39I01N9ZXY/1644494048307/1.jpg",
+    brand:"BVLGARI",
+    price:5650,
+    description:"Omnia Golden Citrine Landia Collection Eau De Toilette",
+
+},
+{
+    img:"https://cdn16.nnnow.com/web-images/preview/styles/X60S4KV5R5W/1610347128815/1.jpg",
+    brand:"BVLGARI",
+    price:9750,
+    description:"Goldea The Roman Night Absolute Eau De Parfum",
+
+},
+{
+    img:"https://cdn03.nnnow.com/web-images/preview/styles/6RA8V6067WY/1610349257779/1.jpg",
+    brand:"BVLGARI",
+    price:8250,
+    description:" Splendida Tubereuse Mystique Eau De Parfum",
+},
+{
+    img:"https://cdn09.nnnow.com/web-images/preview/styles/65U8IT2CI19/1610349230391/1.jpg",
+    brand:"BVLGARI",
+    price:5650,
+    description:" Omnia Pink Sapphire Eau De Toilette",
+},
+{
+    img:"https://cdn08.nnnow.com/web-images/preview/styles/V2XYBPOYLD5/1610347344119/1.jpg",
+    brand:"BVLGARI",
+    price:8500,
+    description:" Rose Goldea Blossom Delight Eau De Parfum ",
+},
+{
+    img:"https://cdn11.nnnow.com/web-images/preview/styles/TF5F88M85XU/1611815971698/1.jpg",
+    brand:"SALVATORE FERRAGAMO",
+    price:3200,
+    description:"Signorina Ribelle Eau De Parfum"
+},
+{
+    img:"https://cdn18.nnnow.com/web-images/preview/styles/YSEXKORYALZ/1643810302317/1.jpg",
+    brand:"SALVATORE FERRAGAMO",
+    price:3500,
+    description:" Ferragamo Intense Leather Eau De Parfum"
+},
+{
+    img:"https://cdn06.nnnow.com/web-images/preview/styles/JRXKQK732QK/1611816067169/1.jpg",
+    brand:"SALVATORE FERRAGAMO",
+    price:4700,
+    description:" Signorina In Fiore Eau De Toilette"
+},
+{
+    img:"https://cdn03.nnnow.com/web-images/preview/styles/MFA1TTS01E2/1611816067213/1.jpg",
+    brand:"SALVATORE FERRAGAMO",
+    price:6800,
+    description:" Amo Flowerful Eau De Toilette",
+}
+]
+
+// var data = JSON.parse(localStorage.getItem("perfumeData")) || [];
+//  displayitem(data);
+
+//  function displayitem(data)
+// {
+//     document.querySelector(".perfume-women").innerHTML="";
+
+
+
+data.map(function (elem){
+ 
+    var box = document.createElement("div");
+
+    var img = document.createElement("img");
+    img.src = elem.img;
+    img.setAttribute("class","img")
+
+    var brand = document.createElement("h3");
+    brand.textContent = elem.brand;
+
+   
+    brand.setAttribute("class","brand")
+
+    var price= document.createElement("h3");
+    price.textContent ="Rs."+elem.price;
+    price.setAttribute("class", "price")
+
+    var description = document.createElement("p");
+    description.textContent = elem.description;
+    description.setAttribute("class", "description")
+    
+    var promotion = document.createElement("p1");
+    promotion.textContent = elem.promotion;
+    promotion.setAttribute("class", "promotion")
+    
+    box.append(img,brand,description,price,promotion);
+    document.querySelector("#container").append(box);
+});
+
